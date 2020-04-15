f9f465630447d66b70fe5e778e7150fbe244f219
deser <deser@DESKTOP-OGI5K9T.mshome.net>
2020-04-15 14:17:06 -0500

    desere

--- src/client/WaitstaffStartPage.java
+++ src/client/WaitstaffStartPage.java
@@ -39,7 +39,7 @@ import java.awt.BorderLayout;
 
 //order doesnt fully function correctly
 
-public class WaitstaffStartPage extends JPanel implements TableModelListener {
+public class WaitstaffStartPage extends JPanel {
 
 	private static final long serialVersionUID = -811294553957L;
 	
