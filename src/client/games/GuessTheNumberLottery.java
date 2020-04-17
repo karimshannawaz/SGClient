b9fc823ad99df3b71b9cfe33337f66d370d94bf6
deser <deser@DESKTOP-OGI5K9T.mshome.net>
2020-04-16 19:20:45 -0500

    adding lottery - desere

---
+++ src/client/games/GuessTheNumberLottery.java
@@ -0,0 +1,157 @@
+package client.games;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import client.Client;
+import client.utils.Constants;
+
+/**
+ * Player will have 7 (because seven guys) tries to guess number, and if number is greater than number guessed,
+ * it will say "too low", or "too high"
+ * 
+ * @author karim
+ *
+ */ 
+public class GuessTheNumberLottery extends JPanel {
+
+
+	private static final long serialVersionUID = 153913217794518944L;
+
+	private StringBuilder output;
+	private JTextField guess;
+	private JLabel triesLbl;
+	private JTextArea textArea;
+
+	public int randomNum = -1;
+	public int tries = 1;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public GuessTheNumberLottery() {
+
+		setBounds(0, 0, 979, 574);
+
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 707, 278);
+		add(scrollPane);
+
+		guess = new JTextField();
+		guess.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		guess.setBounds(81, 323, 116, 38);
+		add(guess);
+		guess.setColumns(10);
+
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				guessNum();
+			}
+		});
+		btnNewButton.setBounds(219, 330, 97, 25);
+		add(btnNewButton);
+
+		output = new StringBuilder();
+
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		output.append("Welcome! Type in your guess to get started. It is a number from 0 to 100. You have seven tries, good luck!\n");
+		textArea.setText(output.toString());
+		textArea.setBounds(81, 32, 707, 278);
+		scrollPane.setViewportView(textArea);
+
+		triesLbl = new JLabel("Tries Left: "+tries);
+		triesLbl.setBounds(698, 323, 90, 27);
+		add(triesLbl);
+	}
+
+
+	protected void guessNum() {
+		int num;
+		try {
+			num = Integer.parseInt(guess.getText());
+		} catch(NumberFormatException e) {
+			Client.sendMessage("Error, number was empty, please enter a valid number between 0 and 100");
+			return;
+		}
+		if(guess.getText().equals("") || guess.getText().equals(null) || num > 5 || num < 1) {
+			Client.sendMessage("Error, number was empty, please enter a valid number between 1 and 5");
+			return;
+		}
+		if(num > randomNum) {
+			Client.sendMessage("Your guess was too high, try again!");
+			output.append("Your guess "+num+" was too high, try again.\n");
+			textArea.setText(output.toString());
+			if(tries == 1) {
+				tries--;
+				triesLbl.setText("Tries left: "+tries);
+				Client.sendMessage("Unfortunately you've run out of tries; the number was: "+randomNum+". Exit this prompt to play again!");
+				output.append("You've run out of tries; the number was: "+randomNum+".\n\n\n");
+				resetGame();
+				return;
+			}
+			tries--;
+			triesLbl.setText("Tries left: "+tries);
+			return;
+		}
+		else if(num < randomNum) {
+			Client.sendMessage("Your guess was too low, try again!");
+			output.append("Your guess "+num+" was too low, try again.\n");
+			textArea.setText(output.toString());
+			if(tries == 1) {
+				tries--;
+				triesLbl.setText("Tries left: "+tries);
+				Client.sendMessage("Unfortunately you've run out of tries; the number was: "+randomNum+". Exit this prompt to play again!");
+				output.append("You've run out of tries; the number was: "+randomNum+".\n\n\n");
+				resetGame();
+				return;
+			}
+			tries--;
+			triesLbl.setText("Tries left: "+tries);
+			return;
+		}
+		tries--;
+		Client.sendMessage("Congratulations, you've guessed the number right! It was "+num+". ");
+		output.append("Congratulations, you've guessed the number right! It was "+num+".\n\n\n");
+		resetGame();
+	}
+
+	private void resetGame() {
+		output.append("Welcome! Type in your guess to get started. It is a number from 1 to 5. You have one try, good luck!\n");
+		textArea.setText(output.toString());
+		tries = 1;
+		guess.setText("");
+		triesLbl.setText("Tries left: "+tries);
+		randomNum = generateNumber(1, 5);
+	}
+
+
+	public static int generateNumber(int minimum, int maximum) {
+		if (minimum > maximum) {
+			return 0;
+		}
+		Random r = new Random();
+		long range = (long) maximum - (long) minimum + 1;
+		long fraction = (long) (range * r.nextDouble());
+		return  ((int) (fraction + minimum));
+	}
+}
---
+++ src/client/games/LotteryPanel.java
@@ -0,0 +1,83 @@
+package client.games;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import client.ClientFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import javax.swing.JTextPane;
+
+public class LotteryPanel extends JPanel {
+
+	/**
+	 * @serial
+	 */
+	private static final long serialVersionUID = -1126922831296889773L;
+
+	public GuessTheNumberLottery gtnPanel;
+	public JPanel mainPanel;
+
+	/**
+	 * Create the panel.
+	 */
+	public LotteryPanel() {
+		super();
+		setBounds(0, 0, 1039, 522);
+		setLayout(null);
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(null);
+		mainPanel.setBounds(0, 0, 1039, 522);
+		add(mainPanel);
+		
+		JButton btnNewButton_1 = new JButton("Guess The Number");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClientFrame.instance.customerSP.openScreen("gtn");
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		btnNewButton_1.setBounds(261, 215, 526, 227);
+		mainPanel.add(btnNewButton_1);
+		
+		JTextPane GamePrompt = new JTextPane();
+		GamePrompt.setEditable(false);
+		GamePrompt.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		GamePrompt.setContentType("text/html");
+		GamePrompt.setText("<html><center><h1><b>This game you will have a 1 in 5 chance to win a free desert.\n </h1></b></p1>\nIf you win, you will be given a free desert coupon to use on your next visit.\r\n</p1><p2>If there are any questions before starting, be sure to hit the \"HELP\" button.</p2></center></html>");
+		GamePrompt.setBounds(50, 90, 933, 118);
+		mainPanel.add(GamePrompt);
+		
+		this.gtnPanel = new GuessTheNumberLottery();
+		add(gtnPanel);
+		gtnPanel.setVisible(false);
+		gtnPanel.randomNum = GuessTheNumber.generateNumber(1, 5);
+		
+	}
+	
+	public void openScreen(String type)
+	{
+		mainPanel.setVisible(false);
+		
+		switch(type) {
+		case "gtn":
+			this.gtnPanel.setVisible(true);
+			break;
+		case "cancel":
+			
+			break;
+			
+		}
+	}
+}
