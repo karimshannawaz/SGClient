19872680dbc765bc6e25b3c1e21dd3a4629f644c
Manasa Nimmagadda <2000manasa13@gmail.com>
2020-04-17 11:54:46 -0500

    client side junit testing -manasa

---
+++ JUnitTests/client/games/GuessTheNumberTest.java
@@ -0,0 +1,31 @@
+package client.games;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GuessTheNumberTest {
+	
+	@BeforeEach
+	void setup() throws Exception
+	{
+		GuessTheNumber tester = new GuessTheNumber();
+	}
+	
+	@Test
+	void testGuessTheNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGuessNum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGenerateNumber() {
+		fail("Not yet implemented");
+	}
+
+}
