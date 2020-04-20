3901117adb8861cb159bcb5ee19a0cfe9d215e74
karimshannawaz <karimshannawaz@my.unt.edu>
2020-04-19 19:56:18 -0500

    change

--- src/client/order/Payment.java
+++ src/client/order/Payment.java
@@ -8,6 +8,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * Contains all of the information needed for a payment panel.
@@ -18,15 +21,23 @@ import javax.swing.JTextArea;
 public class Payment extends JPanel {
 
 	private static final long serialVersionUID = 3521869431085751251L;
-	
+
+	// Represents the State of Texas tax.
 	public double tax = 0.0825;
 
+	// Main Panel components
 	public JPanel mainPanel;
 	public JTextArea orderSummary;
 	public JTextArea orderTotal;
-	private JButton fullBillBtn;
-	private JButton splitBillBtn;
+	public JButton fullBillBtn;
+	public JButton splitBillBtn;
+	public JButton applyDiscountBtn;
+	public JButton removeDiscountBtn;
+	public JComboBox discountsBox;
 
+	/**
+	 * Contructor for the payment panel.
+	 */
 	public Payment() {
 		super();
 		setBounds(0, 0, 1039, 522);
@@ -35,6 +46,9 @@ public class Payment extends JPanel {
 		addMainPanel();
 	}
 
+	/**
+	 * Adds the main payment screen panel.
+	 */
 	private void addMainPanel() {
 		mainPanel = new JPanel();
 		mainPanel.setBounds(0, 0, 1039, 522);
@@ -63,27 +77,102 @@ public class Payment extends JPanel {
 		mainPanel.add(orderTotal);
 
 		JLabel preferenceLbl = new JLabel("<html><div style='text-align: center;'>" + 
-			"How would you like to pay your bill?" + "</div></html>");
+				"How would you like to pay your bill?" + "</div></html>");
 		preferenceLbl.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		preferenceLbl.setBounds(555, 13, 411, 50);
 		mainPanel.add(preferenceLbl);
 
 		fullBillBtn = new JButton("Full Bill");
+		fullBillBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fullBillBtnAction();
+			}
+		});
 		fullBillBtn.setFont(new Font("Arial Black", Font.PLAIN, 55));
-		fullBillBtn.setBounds(512, 76, 515, 200);
+		fullBillBtn.setBounds(512, 76, 515, 148);
 		mainPanel.add(fullBillBtn);
-
+		
 		splitBillBtn = new JButton("Split Bill");
+		splitBillBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				splitBillBtnAction();
+			}
+		});
 		splitBillBtn.setFont(new Font("Arial Black", Font.PLAIN, 55));
-		splitBillBtn.setBounds(512, 309, 515, 200);
+		splitBillBtn.setBounds(512, 228, 515, 148);
 		mainPanel.add(splitBillBtn);
 		
-		fullBillBtn.setVisible(false);
-		splitBillBtn.setVisible(false);
+		applyDiscountBtn = new JButton("Apply Discount!");
+		applyDiscountBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				applyDiscountBtnAction();
+			}
+		});
+		applyDiscountBtn.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		applyDiscountBtn.setBounds(512, 463, 248, 46);
+		mainPanel.add(applyDiscountBtn);
+		
+		removeDiscountBtn = new JButton("Remove Discount");
+		removeDiscountBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeDiscountBtnAction();
+			}
+		});
+		removeDiscountBtn.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		removeDiscountBtn.setBounds(789, 463, 238, 46);
+		mainPanel.add(removeDiscountBtn);
+		
+		JLabel applyDiscntLbl = new JLabel("Would you like to apply a discount?");
+		applyDiscntLbl.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		applyDiscntLbl.setBounds(508, 406, 275, 34);
+		mainPanel.add(applyDiscntLbl);
+		
+		discountsBox = new JComboBox();
+		discountsBox.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		discountsBox.setBounds(779, 400, 248, 50);
+		mainPanel.add(discountsBox);
+
+
+	}
+
+	/**
+	 * Applies a discount to the customer's order
+	 * based on the discount that the customer has in their rewards
+	 * account.
+	 */
+	public void applyDiscountBtnAction() {
+		
 	}
 	
-public void refreshTxtAreas() {
+	/**
+	 * Removes the current discount applied to the
+	 * customer's order.
+	 */
+	public void removeDiscountBtnAction() {
 		
+	}
+
+	/**
+	 * Splits the customer's bill based on items they purchased
+	 */
+	public void splitBillBtnAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Represents the customer paying the full bill at once.
+	 */
+	public void fullBillBtnAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Refreshes the order text area with the correct item and prices.
+	 */
+	public void refreshTxtAreas() {
+
 		StringBuilder s = new StringBuilder();
 		s.append("Order:\n\n");
 
@@ -118,7 +207,7 @@ public void refreshTxtAreas() {
 		orderTotal.setText("Subtotal: "+decimalF(CustomerOrder.subtotal)+"\nTax: "+
 				decimalF(tax * CustomerOrder.subtotal)+"\nTotal: "+decimalF(CustomerOrder.subtotal + (CustomerOrder.subtotal * tax)));
 	}
-	
+
 	/**
 	 * Formats a decimal to be displayed as currency.
 	 * @param num
